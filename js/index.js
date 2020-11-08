9ec5c487f2e54b1d7203f92b563ca9b228b6e2f9
teathpick <158533689@qq.com>
2020-11-08 18:42:51 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+    // 搜索栏部分
+
+    //菜单点击border设置
+    //菜单隐藏
+    $(function(){
+		$(".nav_btn").click(function() {
+			$(".main_list").toggle();
+		});
+  });
+  
+// 二级菜单
+}
